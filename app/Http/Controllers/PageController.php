9d42aad9961dfd509ded1656f46a6505450a748c
vicsystems <victorasuquob@gmail.com>
2025-04-12 09:02:34 +0100

    earnings cards

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -10,7 +10,7 @@ class PageController extends Controller
     //
 
     public function shortUrl(){
-        return redirect()->away('https://docs.google.com/presentation/d/1MtVDBBDmFrzGP2g2hVEIjwFi8FMNjdT20uee-bn0GWo/edit#slide=id.g34b59cf64a4_0_12');
+        return redirect()->away('https://docs.google.com/presentation/d/1MtVDBBDmFrzGP2g2hVEIjwFi8FMNjdT20uee-bn0GWo/edit#slide=id.g34b2edd2127_2_1');
     }
 
     public function index()
